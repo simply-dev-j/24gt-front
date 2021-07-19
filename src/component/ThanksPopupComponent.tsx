37502b37657793ada494c19b3d12f03974f9e883
LeoWu <leow009j@gmail.com>
2021-07-19 11:07:26 +0200

    Added thank popup text messages into i18n resource

--- src/component/ThanksPopupComponent.tsx
+++ src/component/ThanksPopupComponent.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useTranslation, Trans } from 'react-i18next';
+
 export interface ThanksPopupComponentProps {
     onClose:()=>void
 }
@@ -11,13 +13,17 @@ const ThanksPopupComponent:React.FunctionComponent<ThanksPopupComponentProps> = 
         >
             <div className="bg-white px-5 py-10 w-470p sm:w-10/12 rounded">
                 <div className="font-semibold sm:text-18p sm:leading-24p ">
-                    Vielen Dank für Ihre Bestellung.<br/>
-                    Wir werden Ihnen diese schnellstmöglich zusenden.
+                    <Trans
+                        i18nKey="landing.thanks_message"
+                        components={{ br: <br /> }}
+                    />
                 </div>
                 <div className="mt-5 flex">
                     <button className="text-18 font-extrabold text-white px-42p py-2 rounded bg-secondary mx-auto sm:w-full"
                         onClick={() => props.onClose()}>
-                        Close
+                        <Trans
+                            i18nKey="landing.close"
+                        />
                     </button>
                 </div>
             </div>
